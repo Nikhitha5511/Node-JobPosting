f22e7aaaa9c330262cba00aaa4418eba530c36a7
Nikhitha5511 <nikhitha5511@gmail.com>
2024-03-05 18:33:05 +0530

    env added

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// const express = require('express')
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-
-// const jobRoutes = require('./routes/jobapp');
-// console.log(process.env.DB_USERNAME,process.env.DB_PASSWORD);
-// dotenv.config();
-
-// const app=express()
-
-// mongoose.connect("mongodb+srv://<USER>:<PWD>@cluster0.k5t1n1b.mongodb.net/")
-
-// .then(()=>{
-//     console.log("Database Connected successfully");
-// })
-// .catch((err)=>{
-//     console.log("Database conncetion failed");
-// })
-
-// app.use(express.json());
-// app.use("/api/v1/job/",jobRoutes);
-
-
-// app.listen(10000,()=>{
-//     console.log("server running at port 10000");
-// });
-
-
-
 
 const express = require('express');
 const mongoose = require('mongoose');
