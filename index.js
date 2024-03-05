5363e6ed39ee47220bba3c4e9da791abd00b37ca
Nikhitha5511 <nikhitha5511@gmail.com>
2024-03-05 17:57:24 +0530

    dotenv configured

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
-const express = require('express')
-const mongoose = require('mongoose');
+// const express = require('express')
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
 
+// const jobRoutes = require('./routes/jobapp');
+// console.log(process.env.DB_USERNAME,process.env.DB_PASSWORD);
+// dotenv.config();
+
+// const app=express()
+
+// mongoose.connect("mongodb+srv://<USER>:<PWD>@cluster0.k5t1n1b.mongodb.net/")
+
+// .then(()=>{
+//     console.log("Database Connected successfully");
+// })
+// .catch((err)=>{
+//     console.log("Database conncetion failed");
+// })
+
+// app.use(express.json());
+// app.use("/api/v1/job/",jobRoutes);
+
+
+// app.listen(10000,()=>{
+//     console.log("server running at port 10000");
+// });
+
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const jobRoutes = require('./routes/jobapp');
-const app=express()
 
-//mongoose.connect("mongodb+srv://Nikhitha:vijaya5566@V@cluster0.k5t1n1b.mongodb.net/")
-mongoose.connect("mongodb+srv://Nikhitha:vijaya5566%40V@cluster0.k5t1n1b.mongodb.net/")
+dotenv.config();
 
-.then(()=>{
+console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD);
+
+const app = express();
+ 
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.k5t1n1b.mongodb.net/`)
+ .then(() => {
     console.log("Database Connected successfully");
-})
-.catch((err)=>{
-    console.log("Database conncetion failed");
-})
+  })
+  .catch((err) => {
+    console.log("Database connection failed",err);
+  });
 
 app.use(express.json());
-app.use("/api/v1/job/",jobRoutes);
+app.use("/api/v1/job/", jobRoutes);
 
-
-app.listen(10000,()=>{
-    console.log("server running at port 10000");
+app.listen(10000, () => {
+  console.log("Server running at port 10000");
 });
-
-
-
